f6fb75d8a626889483a7347f0908d77171246802
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2023-08-18 22:16:19 -0300

    Testando Branch

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("TESTE GUILHERME");
+
         Scanner sc = new Scanner(System.in);
         int opcao = 0;
 
